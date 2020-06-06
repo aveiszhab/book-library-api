5dbb63092b76eede76451615a4e6938e1cc5c4c9
Aniko Veiszhab <veiszhab.aniko@gmail.com>
2020-06-06 16:37:50 +0100

    Reader password added to model&tests_corrected

--- src/models/reader.js
+++ src/models/reader.js
@@ -2,7 +2,7 @@ module.exports = (sequelize, DataTypes) => {
   const schema = {
     email: DataTypes.STRING,
     name: DataTypes.STRING,
-    password: DataTypes.STRINGgit
+    password: DataTypes.STRING
   };
 
   return sequelize.define('Reader', schema);
